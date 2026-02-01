b5858875e1946fc93d7818b32fb6f671a4257cb0
RiccardoMancino <114989417+RiccardoMancino@users.noreply.github.com>
2026-02-01 22:58:19 +0100

    Fix formatting for PORT variable declaration

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const nodemailer = require("nodemailer");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 3000
+  ;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
